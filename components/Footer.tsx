fecd5e748ddcde4889b36e72b048c9b4db79cce3
Michael Wiradharma <michael.wiradharma@gmail.com>
2025-03-05 22:21:26 -0800

    readme and footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="mt-20 text-black dark:text-white">
       <p>
-        © 2024 Michael Wiradharma. All rights reserved.
+        © {new Date().getFullYear()} Michael Wiradharma. All rights reserved.
       </p>
     </footer>
   );
