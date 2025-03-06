cc53f169535d1f6626a4f43710b65af1d6a9e25f
Michael Wiradharma <michael.wiradharma@gmail.com>
2025-03-05 18:37:25 -0800

    remove unwanted files and updated logo

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
   return (
     <header className="mb-8 py-4">
       <div className="flex flex-row justify-between">
-        <img src="/tandatangan.jpeg" alt="fresh logo" class="w-48 color-gray" />
+        <img src="/tandatangan.jpg" alt="fresh logo" class="w-40 color-gray -translate-y-10" />
         <div className="flex flex-row gap-4">
           {/* <h1 className="text-xl my-4">Work</h1>
           <h1 className="text-xl my-4">About</h1> */}
